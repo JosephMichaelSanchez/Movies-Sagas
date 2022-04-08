b995a80cda59fa304bd2dbd9aa6b5aa30f9d7cec
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-04-08 18:18:37 -0500

    Add button on details to push back to list

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,11 +1,24 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import {useHistory} from 'react-router-dom';
 
 function MovieDetails() {
+
+    const history = useHistory();
+
+    const backToList = () => {
+        console.log('clicked');
+        history.push('/');
+
+    }
+
+    
 
     return (
         <>
             <h2>Movie Details Page</h2>
+
+            <button onClick={backToList}>BACK TO LIST</button>
         </>
     )
 
