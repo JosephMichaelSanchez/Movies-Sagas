4e1838527c1719f2d9f54bc0c498e0aa8f012aba
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-04-08 18:14:21 -0500

    Add push to details page

--- src/components/MovieItem/MovieItem.jsx
+++ src/components/MovieItem/MovieItem.jsx
@@ -1,13 +1,25 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import {useHistory} from 'react-router-dom';
+
 
 function MovieItem({movie}) {
 
+    const history = useHistory();
+
+    
+
+    const getMovieDetails = () => {
+        console.log('clicked');
+        history.push('/details');
+
+        
+    }
     return (
         <>
             <div key={movie.id} >
                 <h3>{movie.title}</h3>
-                <img src={movie.poster} alt={movie.title} />
+                <img onClick={getMovieDetails} src={movie.poster} alt={movie.title} />
             </div>
         </>
 
