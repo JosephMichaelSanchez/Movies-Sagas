9a1fb9a00dbf41303c09c9e9593d8596bcbcc7e1
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-04-08 15:12:57 -0500

    Add MovieDetails component

---
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+function MovieDetails() {
+
+    return (
+        <>
+            <h2>Movie Details Page</h2>
+        </>
+    )
+
+
+}
+
+
+export default MovieDetails;
